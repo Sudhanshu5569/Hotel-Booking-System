554c4396df17e2912034c0451df85c4d05aef9e0
Sudhanshu <sudhanshu4923@gmail.com>
2024-10-23 20:10:06 +0530

    Final upload to Github

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,66 @@
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from ₹1200</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1523699289804-55347c09047d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Comfort Suites Airport</span>
+        <span className="fpCity">Austin</span>
+        <span className="fpPrice">Starting from ₹1400</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1596386461350-326ccb383e9f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Four Seasons Hotel</span>
+        <span className="fpCity">Lisbon</span>
+        <span className="fpPrice">Starting from ₹990</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1657349226767-66c983d7df39?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Hilton Garden Inn</span>
+        <span className="fpCity">Berlin</span>
+        <span className="fpPrice">Starting from ₹1005</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
